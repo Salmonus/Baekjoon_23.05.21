872c8f8363c90e4b234d4f2168d8caeac5f619af
Kim Yeonwoo <70512899+Salmonus@users.noreply.github.com>
2023-05-21 15:04:21 +0900

    [Bronze V] Title: 연세대학교, Time: 204 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+      Scanner myObj = new Scanner(System.in);  
+      int a;
+      a  = myObj.nextInt();
+      if(a == 0){
+          System.out.println("YONSEI");
+      }else{
+          System.out.println("Leading the Way to the Future");
+      }
+    }
+}

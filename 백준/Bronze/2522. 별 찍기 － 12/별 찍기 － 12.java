2a62d964d4c44e0980f6cbe660907004c8dbb2e4
Kim Yeonwoo <70512899+Salmonus@users.noreply.github.com>
2023-05-21 14:27:47 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 592 ms, Memory: 29036 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = 0; i < N; ){
+        i += 1;
+        for(int space = 0; space < N - i; space++) System.out.printf(" ");
+        for(int x = 0; x < i; x++) System.out.printf("*");  
+        System.out.printf("\n");
+    }
+    for(int i = N - 1 ; i > 0; ){
+        for(int space = 0; space < N - i; space++) System.out.printf(" ");
+        for(int x = 0; x < i; x++) System.out.printf("*");  
+        System.out.printf("\n");
+        i -= 1;
+    }
+    }
+}

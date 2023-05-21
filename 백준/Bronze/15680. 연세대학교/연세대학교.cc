76cf6062ec60b66d12839c87c8cb36d5512dca4f
Kim Yeonwoo <70512899+Salmonus@users.noreply.github.com>
2023-05-21 15:04:32 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int S;
+    scanf("%d", &S);
+    if(S==0){
+        printf("YONSEI");
+    }else if(S==1){
+        printf("Leading the Way to the Future");
+    }
+}

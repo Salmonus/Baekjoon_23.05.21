51eaa9edeab282a7fc40c175e51d2fab5a89c809
Kim Yeonwoo <70512899+Salmonus@users.noreply.github.com>
2023-05-21 15:01:27 +0900

    [Bronze V] Title: 사칙연산, Time: 220 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.print(a % b);
+    }
+}

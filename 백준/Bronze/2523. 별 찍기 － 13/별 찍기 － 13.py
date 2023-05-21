fd6110cecad9c1033ab9350c1b4f8ed4fc90adc6
Kim Yeonwoo <70512899+Salmonus@users.noreply.github.com>
2023-05-21 14:29:27 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for i in range(0, N):
+    i += 1
+    for x in range(0, i, 1):
+        print("*", end="")
+    print()
+for i in range(N - 1, 0, -1):
+    for x in range(0, i, 1):
+        print("*", end="")
+    print()

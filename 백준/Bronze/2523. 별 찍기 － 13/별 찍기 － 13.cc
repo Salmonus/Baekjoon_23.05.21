b80d9cd197178f539999d9ef51b6b1480acb21ee
Kim Yeonwoo <70512899+Salmonus@users.noreply.github.com>
2023-05-21 14:28:27 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    for(int i = 0; i < N; ){
+        i += 1;
+        for(int x = 0; x < i; x++) printf("*");  
+        printf("\n");
+    }
+    for(int i = N - 1 ; i > 0; ){
+        for(int x = 0; x < i; x++) printf("*");  
+        printf("\n");
+        i -= 1;
+    }
+}

7e6d61a80bd71571c72bd46a404a7d3f59d7640e
Kim Yeonwoo <70512899+Salmonus@users.noreply.github.com>
2023-05-21 14:25:20 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int N;
+    scanf("%d", &N);
+    for(int i = N; i > 0; ){
+        for(int y = 0; y < N - i; y ++){
+            printf(" ");
+        }
+        for(int x = 0; x < 2*i - 1; x++){
+            printf("*");
+        }
+        printf("\n");
+        i -= 1;
+    }
+    for(int i = 1; i < N; ){
+        i += 1;
+        for(int y = 0; y < N - i; y ++){
+            printf(" ");
+        }
+        for(int x = 0; x < 2*i - 1; x++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}

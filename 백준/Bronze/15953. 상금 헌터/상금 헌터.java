a37c9d353fd6e16d90189bc0c11f19aa26906af1
WitHub <70512899+Salmonus@users.noreply.github.com>
2023-07-07 14:32:24 +0900

    [Bronze III] Title: 상금 헌터, Time: 492 ms, Memory: 25968 KB -BaekjoonHub

---
+++ 백준/Bronze/15953. 상금 헌터/상금 헌터.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+      int T, a, b, fst, snd;
+      Scanner s = new Scanner(System.in);
+      T = s.nextInt();
+      
+      for(int i=0; i < T; i++){
+          a = s.nextInt();
+          b = s.nextInt();
+        if (a == 1){
+            fst = 500;
+        }else if((3 >= a) && (a >= 2)){
+            fst = 300;
+        }else if(6 >= a && a >= 4){
+            fst = 200;
+        }else if(10 >= a && a >= 7){
+            fst = 50;
+        }else if(15 >= a && a >= 11){
+            fst = 30; 
+        }else if(21 >= a && a >= 16){
+            fst = 10;
+        }else{
+            fst = 0;
+        }
+        
+        if (b == 1){
+            snd = 512;
+        }else if(3 >= b && b >= 2){
+            snd = 256;
+        }else if(7 >= b && b >= 4){
+            snd = 128;
+        }else if(15 >= b && b >= 8){
+            snd = 64;
+        }else if(31 >= b && b >= 16){
+            snd = 32; 
+        }else{
+            snd = 0;
+        }
+
+      System.out.printf("%d\n", fst * 10000 + snd * 10000);
+    }
+}}

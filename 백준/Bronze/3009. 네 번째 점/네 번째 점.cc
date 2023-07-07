391496ec12ea69f40009822b4ed63befab1169d4
WitHub <70512899+Salmonus@users.noreply.github.com>
2023-07-07 12:08:55 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int x1, x2, x3, x4, y1, y2, y3, y4;
+    scanf("%d %d", &x1, &y1);
+    scanf("%d %d", &x2, &y2);
+    scanf("%d %d", &x3, &y3);
+    
+    x4 = x1 == x3 ? x2 : (x1 == x2 ? x3 : x1);
+    y4 = y1 == y3 ? y2 : (y1 == y2 ? y3 : y1);
+    
+    printf("%d %d", x4, y4);
+}

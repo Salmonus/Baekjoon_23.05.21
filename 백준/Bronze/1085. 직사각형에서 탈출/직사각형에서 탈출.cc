f52ad43e001802d5dfecf17501b3019ff4046b58
WitHub <70512899+Salmonus@users.noreply.github.com>
2023-07-07 14:56:17 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int x, y, w, h;
+    
+    scanf("%d %d %d %d", &x, &y, &w, &h);
+    
+    int min_x = x < w - x ? x : w - x;
+    int min_y = y < h - y ? y : h - y;
+    
+    printf("%d", min_x < min_y ? min_x : min_y);
+}
